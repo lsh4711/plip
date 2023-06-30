86ba6c6285cf5d13358abf612f3b94d1e2c89dc2
xion <calf-love@naver.com>
2023-06-30 14:28:11 +0900

    :adhesive_bandage: Create index.ts
    for easy import

---
+++ client/src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Button } from './atom/Button';
+export { default as HeadingParagraph } from './atom/HeadingParagraph';
+export { default as Input } from './atom/Input';
+export { default as Paragraph } from './atom/Paragraph';
