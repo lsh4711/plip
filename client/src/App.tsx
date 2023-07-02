7b15cab6c6e4775eaae67dd3bbcb19bc0c88ecf4
xion <calf-love@naver.com>
2023-07-02 17:08:45 +0900

    :fire: App.tsx clean
    cleaning

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,7 @@
-import { Button, HeadingParagraph, Input } from './components';
-
 export default function App() {
   return (
     <main className="flex flex-col items-center justify-center overflow-x-hidden py-24">
-      <div className=" flex">
-        <Button>안녕하세요</Button>
-        <Button
-          variant="primary"
-          hovercolor={'active'}
-          hoveropacity="active"
-          activecolor={'active'}
-        >
-          Sign up
-        </Button>
-        <Button variant={'ring'}>Sign in</Button>
-        <Button size={'lg'} variant="primary">
-          Sign up
-        </Button>
-      </div>
-      <div className="flex flex-col">
-        <Input placeholder="email" />
-      </div>
-      <div className=" flex flex-col">
-        <HeadingParagraph size={'xl'} variant="gray">
-          이번 국내 여행,
-          <br /> 보다 알찬 여행으로 만들고 싶다면? <br /> GitHub로 여행 계획을 디자인해 보세요
-        </HeadingParagraph>
-        <HeadingParagraph variant={'blue'}>어엄</HeadingParagraph>
-      </div>
+      성지현그녀는감히전설이라고할수있다
     </main>
   );
 }
