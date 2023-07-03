3b441fc2a741a8c739cc57a305f4702cdd8a4607
dayoungee <55998706+dayoungee@users.noreply.github.com>
2023-07-03 11:51:25 +0900

    [Server][FEAT] 회원가입 API 작성 (#34)
    * feat: security config Add
    - Rest API이기 떄문에 formlogin, httpbasic 설정 우선 주석처리
    - cors 설정(모든 옵션 열어둠)
    Related to: #5
    
    * feat: 회원가입 기본 API 구현
    
    - 예외처리(중복 확인), 데이터 검증 추가해야함
    - 비밀번호 암호화 추가해야함
    - 정말 기본적인 create만 구현한 상태
    - h2 접근 권한 문제로 SecurityConfig에 설정 추가
    
    Related to: #5
    
    * refactor: MemberController 리팩토링
    
    - 추후에 추가될 createUri 공통 메소드를 위해 기존 포맷 그대로 수정
    
    Related to: #5
    
    * feat: 회원가입 예외처리 추가
    
    - 비밀번호 암호화 추가
    - 이메일, 닉네임, 비밀번호 데이터 검증 처리 추가
    - 이메일, 닉네임 중복 검사
    
    Related to: #5
    
    * test: 회원가입 API 테스트 코드, API 문서화 추가
    
    - 회원가입 API에 대한 테스트 코드 작성
    - API 문서화 작성
    
    Related to: #5

---
+++ server/src/main/java/com/server/global/config/SecurityConfig.java
@@ -0,0 +1,59 @@
+package com.server.global.config;
+
+import static org.springframework.security.config.Customizer.*;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity(debug = false)
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .headers().frameOptions().sameOrigin()
+            .and()
+            .csrf().disable()
+            .cors(withDefaults())
+            .exceptionHandling()
+            .and()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .authorizeRequests()
+            .anyRequest().permitAll();
+        //.formLogin().disable()
+        //.httpBasic().disable()
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedMethods(List.of("*"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setExposedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
