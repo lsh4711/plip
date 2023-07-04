26a37bbcc08f625d42bce78bc3ea13d3e41f1f2f
jeein2222 <jiinbag057@gmail.com>
2023-07-04 13:46:37 +0900

    refactor: Record Stubdata 추가

--- server/src/test/java/com/server/helper/StubData.java
+++ server/src/test/java/com/server/helper/StubData.java
@@ -1,11 +1,13 @@
 package com.server.helper;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.http.HttpMethod;
 
 import com.server.domain.member.dto.MemberDto;
+import com.server.domain.record.dto.RecordDto;
 
 public class StubData {
     public static class MockMember {
@@ -19,6 +21,33 @@ public class StubData {
                 .nickname("테스트계정")
                 .build();
             stubRequestBody.put(HttpMethod.POST, post);
+        }
+
+        public static Object getRequestBody(HttpMethod method) {
+            return stubRequestBody.get(method);
+        }
+    }
+
+    public static class MockRecord{
+        private static Map<HttpMethod, Object> stubRequestBody;
+
+        static{
+            stubRequestBody = new HashMap<>();
+            RecordDto.Post post = RecordDto.Post.builder()
+                .title("서울 롯데월드")
+                .content("롯데월드에서는..")
+                .build();
+            stubRequestBody.put(HttpMethod.POST, post);
+
+            RecordDto.Response response = RecordDto.Response.builder()
+                .recordId(1L)
+                .title("서울 롯데월드")
+                .content("롯데월드에서는..")
+                .memberId(1L)
+                .createdAt(LocalDateTime.now().withNano(0))
+                .modifiedAt(LocalDateTime.now().withNano(0))
+                .build();
+            stubRequestBody.put(HttpMethod.GET, response);
         }
 
         public static Object getRequestBody(HttpMethod method) {
