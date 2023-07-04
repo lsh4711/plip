74cb948eeedc0ba6501666a06f81906b4d548bab
xion <calf-love@naver.com>
2023-07-04 16:54:31 +0900

    :bug: Error message fix
    for signup

--- client/src/pages/SignUpPage.tsx
+++ client/src/pages/SignUpPage.tsx
@@ -166,16 +166,17 @@ const SignUpPage = () => {
 };
 
 let passwordRegex = /(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\W)(?=\S+$).{8,16}/;
-
+let nicknameRegex = /^[ㄱ-ㅎ가-힣a-z0-9-_]{2,10}$/;
 const signupSchema = z
   .object({
     email: z.string().nonempty().email({ message: '유효하지 않은 이메일 양식입니다.' }),
     authnumber: z.string().optional(),
     nickname: z
       .string()
-      .nonempty()
+      .nonempty({ message: '닉네임은 필수 입력입니다.' })
       .min(2, { message: '닉네임은 2글자 이상 10글자 이하여야합니다.' })
-      .max(10, { message: '닉네임은 2글자 이상 10글자 이하여야합니다.' }),
+      .max(10, { message: '닉네임은 2글자 이상 10글자 이하여야합니다.' })
+      .regex(nicknameRegex, { message: '닉네임에 특수문자는 포함될 수 없습니다.' }),
     password: z.string().nonempty({ message: '비밀번호는 필수 입력입니다.' }).regex(passwordRegex, {
       message: '비밀번호는 영문,숫자,특수문자를 모두 포함한 8자 이상이어야 합니다.',
     }),
