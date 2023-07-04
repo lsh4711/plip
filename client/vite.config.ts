686c112586ea2a5d2525ddb3590fd428cf814848
22yuu <22yuu@naver.com>
2023-07-04 18:20:40 +0900

    feat: Header 컴포넌트 구현 완료

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,12 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
 import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
