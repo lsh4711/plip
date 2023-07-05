828ef48773b464599373557ca0f62ede3c02d136
xion <calf-love@naver.com>
2023-07-05 15:57:19 +0900

    test

--- client/src/pages/MyRecordPage.tsx
+++ client/src/pages/MyRecordPage.tsx
@@ -2,9 +2,9 @@ interface MyRecordPageProps {}
 
 const MyRecordPage = ({}: MyRecordPageProps) => {
   return (
-    <div className=" flex w-full items-center justify-center">
+    <div className=" flex w-full">
       <nav className=" sticky right-0 top-0 h-screen border-r border-zinc-400">하이욤</nav>
-      <main className=" flex w-full max-w-[1024px] items-center justify-center">엄준식</main>
+      <main className="">엄준식</main>
     </div>
   );
 };
