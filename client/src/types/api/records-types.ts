9b25b384801c6325e4869fe2f60e286a66c205f2
xion <calf-love@naver.com>
2023-07-06 11:57:22 +0900

    :sparkles: Api typing
    email / places / records / schedules / users

---
+++ client/src/types/api/records-types.ts
@@ -0,0 +1,37 @@
+//POST : /records
+export interface RecordRequest {
+  title: string;
+  content: string;
+}
+
+//GET : /records/{record-id}
+export interface RecordGetRequest {
+  recordId: number | string;
+}
+
+export interface Records {
+  recordId: number | string;
+  title: string;
+  content: string;
+  memberId: number | string;
+  createdAt: string | Date;
+  modifiedAt: string | Date;
+}
+
+//GET : /records/{record-id}
+export interface RecordGetResponse {
+  data: Records;
+}
+
+//GET : /records?page=1&size=10
+
+export interface GetRecordsBymemberIdResponse extends Records {}
+
+//GET : /records/place/{place-id}
+export interface GetRecordsByPlaceIdRequest {
+  'place-id': string | number;
+}
+
+//GET : /records/place/{place-id}
+
+export interface GetRecordsByPlaceIdResponse extends Records {}
---
+++ client/src/types/api/users-types.ts
@@ -0,0 +1,29 @@
+/*
+UserLoginResponse Header
+"Authorization:Bearer {access-token}"
+"Refresh {refresh-token}" 
+*/
+
+//POST : /users/signup
+export interface UsersSignupRequest {
+  email: string;
+  password: string;
+  nickname: string;
+}
+
+//POST : /users/login
+export interface UserLoginRequest {
+  username: string;
+  password: string;
+}
+
+//GET : /users/{user-id}
+export interface UserGetRequest {
+  nickname: string;
+}
+
+//PATCH : /users
+export interface UserPatchRequest {
+  nickname: string;
+  password: string;
+}
