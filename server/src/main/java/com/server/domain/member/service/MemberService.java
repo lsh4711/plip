5bfeba36c893dacfa5e6a32812195d6b75dba215
jeein2222 <jiinbag057@gmail.com>
2023-07-06 09:17:16 +0900

    chore: 등록된 회원인지 검증하는 메서드 추가

--- server/src/main/java/com/server/domain/member/service/MemberService.java
+++ server/src/main/java/com/server/domain/member/service/MemberService.java
@@ -1,5 +1,7 @@
 package com.server.domain.member.service;
 
+import java.util.Optional;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -7,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.server.domain.member.entity.Member;
 import com.server.domain.member.repository.MemberRepository;
 
+import com.server.global.exception.BusinessLogicException;
 import com.server.global.exception.CustomException;
 import com.server.global.exception.ExceptionCode;
 
@@ -39,5 +42,15 @@ public class MemberService {
     public void checkExistEmail(Member member) {
         if (memberRepository.existsByEmail(member.getEmail()))
             throw new CustomException(ExceptionCode.EMAIL_EXISTS);
+    }
+
+    //회원이메일로 등록된 회원인지 검증
+    public Member findVerifiedMember(String email){
+        Optional<Member> optionalMember =
+            memberRepository.findByEmail(email);
+        Member findMember =
+            optionalMember.orElseThrow(() ->
+                new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
+        return findMember;
     }
 }
