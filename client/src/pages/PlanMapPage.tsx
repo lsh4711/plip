1c708fea58eb124c16de061a6c5eb9fea3265fde
sungjihyun <jhsung23@gmail.com>
2023-07-09 03:52:58 +0900

    feat: 여행 계획 페이지에서 side panel 보여주기

--- client/src/pages/PlanMapPage.tsx
+++ client/src/pages/PlanMapPage.tsx
@@ -1,7 +1,41 @@
-interface PlanMapPageProps {}
+import SidePanel from '@/components/common/SidePanel';
+import TripInfo from '@/components/common/TripInfo';
+import TripSchedule from '@/components/common/TripSchedule';
+import { regions } from '@/datas/regions';
 
-const PlanMapPage = ({}: PlanMapPageProps) => {
-  return <div>PlanMapPage</div>;
+export type ResponseData = {
+  title: string | null;
+  region: (typeof regions)[number];
+  startDate: Date;
+  endDate: Date;
+  places: any;
+};
+
+const PlanMapPage = () => {
+  const responseData: ResponseData = {
+    title: null,
+    region: 'seoul',
+    startDate: new Date(),
+    endDate: new Date(),
+    places: [
+      [{ placeName: '인천 국제 공항' }, { placeName: '버스터미널' }],
+      [{ placeName: '평창역' }, { placeName: '사근진 해변' }],
+    ],
+  };
+
+  return (
+    <div>
+      <SidePanel position={'right'}>
+        <TripInfo
+          title={responseData.title}
+          region={responseData.region}
+          startDate={responseData.startDate}
+          endDate={responseData.endDate}
+        />
+        <TripSchedule startDate={responseData.startDate} places={responseData.places} />
+      </SidePanel>
+    </div>
+  );
 };
 
 export default PlanMapPage;
