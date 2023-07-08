e23a184dabe5a8d3326024a0148b9140e7a6be11
sungjihyun <jhsung23@gmail.com>
2023-07-08 23:27:53 +0900

    feat: side panel 컴포넌트

---
+++ client/src/components/common/SidePanel.tsx
@@ -0,0 +1,55 @@
+import { cn } from '@/utils';
+import { cva } from 'class-variance-authority';
+import { useState } from 'react';
+import SideToggleButton from './SideToggleButton';
+
+export const SidePanelVariants = cva(
+  `
+  opening fixed h-screen w-96 bg-white drop-shadow-2xl    
+  `,
+  {
+    variants: {
+      position: {
+        left: ' left-0',
+        right: ' right-0',
+      },
+      close: {
+        true: '',
+        false: '',
+      },
+    },
+    compoundVariants: [
+      {
+        position: 'left',
+        close: true,
+        class: '-translate-x-full',
+      },
+      {
+        position: 'right',
+        close: true,
+        class: 'translate-x-full',
+      },
+    ],
+    defaultVariants: {
+      position: 'right',
+    },
+  }
+);
+
+interface SidePanelProps {
+  position: 'left' | 'right';
+  children: React.ReactNode;
+}
+
+function SidePanel({ position, children }: SidePanelProps) {
+  const [isOpen, setIsOpen] = useState(true);
+
+  return (
+    <div className={cn(SidePanelVariants({ position, close: !isOpen }))}>
+      <SideToggleButton isOpen={isOpen} position={position} onClick={() => setIsOpen(!isOpen)} />
+      {children}
+    </div>
+  );
+}
+
+export default SidePanel;
