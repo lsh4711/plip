32e3754560afaeb3e7fd0265ec1b7bee6391256e
22yuu <22yuu@naver.com>
2023-07-09 15:17:34 +0900

    feat: 마이페이지 유효성 검사 이메일, 닉네임

--- client/src/pages/MyPage.tsx
+++ client/src/pages/MyPage.tsx
@@ -1,9 +1,12 @@
 import { Button, HeadingParagraph, Input, Paragraph } from '@/components';
-import { useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 import { ReactComponent as EditImage } from '@/assets/icons/camera.svg';
 import { ReactComponent as EditIcon } from '@/assets/icons/edit.svg';
-import { ReactComponent as CloseIcon } from '@/assets/icons/close.svg';
-import { ReactComponent as DoneIcon } from '@/assets/icons/check.svg';
+
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { passwordRegex, nicknameRegex } from '@/datas/constants';
 
 import { Link } from 'react-router-dom';
 
@@ -11,12 +14,31 @@ import Avatar from '@/components/common/Avatar';
 interface MyPageProps {}
 
 const MyPage = ({}: MyPageProps) => {
-  const nickname = '전설유길종전설성지현'; // 임시 변수
+  const editForm = useForm<EditProfileTypes>({
+    mode: 'all',
+    resolver: zodResolver(profileSchema),
+  });
+
+  const onSubmit: SubmitHandler<EditProfileTypes> = (data) => {
+    console.log(data);
+  };
+
+  // is edit?
   const [isEditNickName, setIsEditNickName] = useState(false);
   const [isEditEmail, setIsEditEmail] = useState(false);
   const [isRequestedEmailAuthentication, setIsRequestedEmailAuthentication] = useState(false);
 
+  // valid
+  const [isEmailValid, setIsEmailValid] = useState(true);
+  const [isNicknameValid, setIsNicknameValid] = useState(true);
+
+  // 임시 상태 변수
   const [userEmail, setUserEmail] = useState('ryubogum@plip.io');
+  const [userNickname, setUserNickName] = useState('전설유길종전설성지현');
+
+  const onChangeNickNameHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setUserNickName(e.target.value);
+  };
 
   const onEditHandler = (
     state: boolean,
@@ -33,7 +55,10 @@ const MyPage = ({}: MyPageProps) => {
           프로필 수정
         </HeadingParagraph>
         <div className="flex justify-center">
-          <form className="flex w-[500px] flex-col gap-y-6">
+          <form
+            className="flex w-[500px] flex-col gap-y-6"
+            onSubmit={editForm.handleSubmit(onSubmit)}
+          >
             {/* edit avatar & nickname */}
             <div className="flex items-center gap-x-6">
               <div className="relative">
@@ -47,58 +72,49 @@ const MyPage = ({}: MyPageProps) => {
                   </Link>
                 </Button>
               </div>
-              <div className="flex">
-                <Input
-                  value={nickname}
-                  placeholder="변경할 닉네임을 작성해주세요."
-                  className="h-[52px]"
-                  disabled={!isEditNickName}
-                />
-                <div>
-                  {!isEditNickName ? (
-                    <Button
-                      type="button"
-                      hovercolor={'default'}
-                      onClick={() => onEditHandler(isEditNickName, setIsEditNickName)}
-                    >
-                      <EditIcon width={24} height={24} />
-                    </Button>
-                  ) : (
-                    <div className="ml-[10px] flex items-center gap-4">
-                      {/* <Button
-                        type="button"
-                        hovercolor={'default'}
-                        onClick={onEditNickNameHandler}
-                        className="p-0"
-                      >
-                        <CloseIcon />
-                      </Button>
+              <div>
+                <div className="flex">
+                  <Input
+                    value={userNickname}
+                    placeholder="변경할 닉네임을 작성해주세요."
+                    className="h-[52px]"
+                    disabled={!isEditNickName}
+                    {...editForm.register('nickname', { onChange: onChangeNickNameHandler })}
+                  />
+                  <div>
+                    {!isEditNickName ? (
                       <Button
                         type="button"
-                        hovercolor={'default'}
-                        onClick={onEditNickNameHandler}
-                        className="p-0"
-                      >
-                        <DoneIcon />
-                      </Button> */}
-                      <Button
-                        type="button"
-                        variant={'ring'}
                         hovercolor={'default'}
                         onClick={() => onEditHandler(isEditNickName, setIsEditNickName)}
                       >
-                        취소
+                        <EditIcon width={24} height={24} />
                       </Button>
-                      <Button
-                        type="button"
-                        variant={'primary'}
-                        onClick={() => onEditHandler(isEditNickName, setIsEditNickName)}
-                      >
-                        완료
-                      </Button>
-                    </div>
-                  )}
+                    ) : (
+                      <div className="ml-[10px] flex items-center gap-4">
+                        <Button
+                          type="button"
+                          variant={'ring'}
+                          hovercolor={'default'}
+                          onClick={() => onEditHandler(isEditNickName, setIsEditNickName)}
+                        >
+                          취소
+                        </Button>
+                        <Button
+                          type="button"
+                          variant={'primary'}
+                          onClick={() => onEditHandler(isEditNickName, setIsEditNickName)}
+                        >
+                          완료
+                        </Button>
+                      </div>
+                    )}
+                  </div>
                 </div>
+                <Paragraph variant={'red'} size="xs" className=" mt-1">
+                  <p> {isNicknameValid ? null : '중복된 닉네임입니다.'}</p>
+                  <p> {editForm.formState.errors.nickname?.message}</p>
+                </Paragraph>
               </div>
             </div>
 
@@ -109,6 +125,12 @@ const MyPage = ({}: MyPageProps) => {
                 placeholder="이메일을 입력해 주세요."
                 className="w-[300px]"
                 disabled={!isEditEmail}
+                {...editForm.register('email', {
+                  onChange: (event: React.ChangeEvent<HTMLInputElement>) => {
+                    const nowdata = editForm.getValues('email');
+                    console.log(nowdata);
+                  },
+                })}
               />
               {!isEditEmail ? (
                 <Button
@@ -150,6 +172,11 @@ const MyPage = ({}: MyPageProps) => {
                   </Button>
                 </>
               )}
+              <Paragraph variant={'red'} size="xs" className=" mt-1">
+                {editForm.formState.errors.email?.message && editForm.getValues('email') !== ''
+                  ? editForm.formState.errors.email.message
+                  : null}
+              </Paragraph>
             </div>
 
             {isRequestedEmailAuthentication && (
@@ -210,5 +237,32 @@ const MyPage = ({}: MyPageProps) => {
     </div>
   );
 };
+
+const profileSchema = z
+  .object({
+    email: z.string().nonempty().email({ message: '유효하지 않은 이메일 양식입니다.' }),
+    authnumber: z.string().optional(),
+    nickname: z
+      .string()
+      .nonempty({ message: '닉네임은 필수 입력입니다.' })
+      .min(2, { message: '닉네임은 2글자 이상 10글자 이하여야합니다.' })
+      .max(10, { message: '닉네임은 2글자 이상 10글자 이하여야합니다.' })
+      .regex(nicknameRegex, { message: '닉네임에 특수문자는 포함될 수 없습니다.' }),
+    password: z.string().nonempty({ message: '비밀번호는 필수 입력입니다.' }).regex(passwordRegex, {
+      message: '비밀번호는 영문,숫자,특수문자를 모두 포함한 8자 이상이어야 합니다.',
+    }),
+    checkpassword: z.string(),
+  })
+  .superRefine(({ checkpassword, password }, ctx) => {
+    if (checkpassword !== password) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: '비밀번호가 서로 일치하는지 확인 해주세요',
+        path: ['checkpassword'],
+      });
+    }
+  });
+
+export type EditProfileTypes = z.infer<typeof profileSchema>;
 
 export default MyPage;
