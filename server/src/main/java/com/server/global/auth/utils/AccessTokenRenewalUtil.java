b5d45c850cfc1bd257e6d0fdcf2e8d7a29b90ca5
Dayoung <dbsek21@naver.com>
2023-07-11 15:05:08 +0900

    refactor: 레디스 사용없이 쿠키로 accesstoken 재발급
    - 레디스로 검증을 할지는 생각해봐야할듯

--- server/src/main/java/com/server/global/auth/utils/AccessTokenRenewalUtil.java
+++ server/src/main/java/com/server/global/auth/utils/AccessTokenRenewalUtil.java
@@ -1,5 +1,7 @@
 package com.server.global.auth.utils;
 
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Component;
@@ -13,6 +15,8 @@ import com.server.global.auth.jwt.JwtTokenizer;
 import com.server.global.exception.CustomException;
 import com.server.global.exception.ExceptionCode;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -23,26 +27,21 @@ public class AccessTokenRenewalUtil {
     private final DelegateTokenUtil delegateTokenUtil;
     private final JwtTokenizer jwtTokenizer;
 
-    // TODO: 재발급을 쿠키에 있는 걸로 쓸지, 레디스로 검증을 한번 할지 물어보고 리팩토링
+    // TODO: 쿠키 재발급으로 변경, 레디스로 검증을 한번 할지 물어보고 리팩토링
     public Token renewAccessToken(HttpServletRequest request) {
         try{
-            String accessToken = jwtTokenizer.getHeaderAccessToken(request);
-            String userRefreshToken = jwtTokenizer.getHeaderRefreshToken(request);
-
-            RefreshToken findRefreshToken = refreshTokenService.getTokenByAccessToken(accessToken);
-            String redisRefreshToken = findRefreshToken.getRefreshToken();
-            long memberId = Long.parseLong(findRefreshToken.getId());
-            Member member = memberRepository.findById(memberId)
-                .orElseThrow(() -> new CustomException(ExceptionCode.MEMBER_NOT_FOUND));
+            String refreshToken = jwtTokenizer.getHeaderRefreshToken(request);
+            String base64EncodedSecretKey = jwtTokenizer.encodeBase64SecretKey(jwtTokenizer.getSecretKey());
+            String email = jwtTokenizer.getClaims(refreshToken, base64EncodedSecretKey).getBody().getSubject();
+            Member member = memberRepository.findByEmail(email).orElseThrow(() -> new CustomException(ExceptionCode.MEMBER_NOT_FOUND));
             String newAccessToken = delegateTokenUtil.delegateAccessToken(member);
-
-            refreshTokenService.saveTokenInfo(memberId, redisRefreshToken, newAccessToken);
             return Token.builder()
                 .accessToken(newAccessToken)
-                .refreshToken(redisRefreshToken)
+                .refreshToken(refreshToken)
                 .build();
         }catch (CustomException ce){
             throw ce;
         }
     }
+
 }
