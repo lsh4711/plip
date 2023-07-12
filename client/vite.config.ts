ced6ad4a7576b0176fe45cf0e0b41552c1744754
xion <calf-love@naver.com>
2023-07-12 13:44:21 +0900

    optimize js bundle size for lighthous

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,20 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { compression } from 'vite-plugin-compression2';
 import svgr from 'vite-plugin-svgr';
 
 import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    compression({
+      include: [/\.(js)$/, /\.(css)$/, /\.(webp)$/],
+      threshold: 1400,
+    }),
+  ],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
