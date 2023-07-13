9c134f025bd4c8759760d73a42e9766f1f371c6f
Dayoung <dbsek21@naver.com>
2023-07-13 13:40:57 +0900

    chore: 여행일지 get 빼고 모든 api 인증권한 부여

--- server/src/main/java/com/server/global/config/SecurityConfig.java
+++ server/src/main/java/com/server/global/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -69,6 +70,14 @@ public class SecurityConfig {
             .and()
             .authorizeHttpRequests(authorize -> authorize
                 .antMatchers("/*/users").authenticated()
+
+                .antMatchers(HttpMethod.GET, "/*/records/*").permitAll()
+                .antMatchers("/*/records").authenticated()
+                .antMatchers("/*/records/**").authenticated()
+
+                .antMatchers("/*/places/**").authenticated()
+                .antMatchers("/*/schedules/**").authenticated()
+
                 .anyRequest().permitAll());
 
         return http.build();
