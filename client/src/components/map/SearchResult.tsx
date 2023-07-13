e4d79456426a2c5389d1c81b90e79b7eba1bff04
sungjihyun <jhsung23@gmail.com>
2023-07-13 16:36:16 +0900

    feat: 키워드 및 카테고리 검색, 검색 결과 무한 스크롤

--- client/src/components/map/SearchResult.tsx
+++ client/src/components/map/SearchResult.tsx
@@ -1,6 +1,10 @@
+import useIntersectionObserver from '@/hooks/useIntersectionObserver';
+import {
+  SearchPlaceByCategory,
+  SearchPlaceByKeyword,
+  default as useSearchPlaceQuery,
+} from '@/queries/search/useKeywordSearchQuery';
 import { MdClose } from '@react-icons/all-files/md/MdClose';
-
-import { GetPlaceByKeywordResponse } from '@/types/mapApi/place-types';
 
 type ResultInfoProps = {
   children: React.ReactNode;
@@ -8,27 +12,29 @@ type ResultInfoProps = {
 
 const ResultInfo = ({ children }: ResultInfoProps) => {
   return (
-    <div className="item fixed flex w-full items-center justify-between gap-1 px-4 py-2 text-xs text-[#343539]">
+    <div className="item sticky left-0 top-0 flex w-full items-center justify-between gap-1 border-b-[1px] bg-white px-4 py-2 text-xs text-[#343539]">
       {children}
     </div>
   );
 };
 
-interface ResultItemProps {
+type ResultItemProps = {
   order: number;
   placeName: string;
   address: string;
   category: string;
   phone: string;
-}
+};
 
 const ResultItem = ({ order, placeName, address, category, phone }: ResultItemProps) => {
   return (
     <div className="flex flex-col gap-1 border-t-[1px] border-solid px-4 py-2">
       <div className="flex items-center gap-2">
         <span className="font-extrabold text-[#4568DC]">{order + 1}</span>
-        <span className="font-bold">{placeName}</span>
-        <span className="text-xs text-[#bbb]">{category || ''}</span>
+        <span className="overflow-hidden text-ellipsis whitespace-nowrap font-bold">
+          {placeName}
+        </span>
+        <span className="shrink-0 text-xs text-[#bbb]">{category || ''}</span>
       </div>
       <span className="text-xs">{address}</span>
       <span className={`text-xs ${phone ? 'text-[#343539]' : 'text-[#bbb]'}`}>
@@ -38,52 +44,74 @@ const ResultItem = ({ order, placeName, address, category, phone }: ResultItemPr
   );
 };
 
-type SearchResultProps = {
-  keyword: string;
-  results:
-    | Pick<
-        GetPlaceByKeywordResponse,
-        'place_name' | 'road_address_name' | 'phone' | 'id' | 'category_name'
-      >[]
-    | null;
-  onResetSearch: () => void;
-};
+type SearchResultProps = SearchPlaceByCategory &
+  SearchPlaceByKeyword & {
+    onResetSearch: () => void;
+  };
 
-const SearchResult = ({ keyword, results, onResetSearch }: SearchResultProps) => {
+const SearchResult = ({
+  keyword,
+  onResetSearch,
+  currentX,
+  currentY,
+  categoryCode,
+}: SearchResultProps) => {
+  const { data, fetchNextPage, isFetchingNextPage, hasNextPage, error, isLoading } =
+    useSearchPlaceQuery({
+      keyword,
+      currentX,
+      currentY,
+      categoryCode,
+    });
+
+  const { ref } = useIntersectionObserver(() => {
+    if (hasNextPage) fetchNextPage();
+  });
+
+  const pagination = data?.pages[0].pagination;
+
   return (
     <>
-      {results && (
-        <div className="pointer-events-auto left-0 top-12 min-h-fit w-80 overflow-y-scroll rounded-lg border-[1px] border-solid border-[#bbb] bg-white drop-shadow-lg">
-          <ResultInfo>
-            <div>
-              <span className="mr-1 inline-block max-w-[8rem] overflow-hidden text-ellipsis align-top font-bold text-[#4568DC]">
-                {keyword}
-              </span>
-              검색 결과
-              <span> {results.length}</span>건
-            </div>
-            <MdClose size={14} color="#bbb" onClick={onResetSearch} className="cursor-pointer" />
-          </ResultInfo>
-          <div className="mt-8 h-fit">
-            {results.length ? (
-              results.map(({ id, place_name, road_address_name, category_name, phone }, idx) => (
-                <ResultItem
-                  key={id}
-                  order={idx}
-                  placeName={place_name}
-                  address={road_address_name}
-                  category={category_name}
-                  phone={phone}
-                />
-              ))
-            ) : (
-              <div className="flex items-center justify-center border-t-[1px] border-solid border-[#bbb] p-4 text-sm text-[#bbb]">
-                검색 결과가 없습니다.
-              </div>
-            )}
+      <div className="pointer-events-auto relative left-0 min-h-fit w-80 overflow-y-scroll rounded-lg border-[1px] border-solid border-[#bbb] bg-white drop-shadow-lg">
+        <ResultInfo>
+          <div>
+            <span className="mr-1 inline-block max-w-[8rem] overflow-hidden text-ellipsis align-top font-bold text-[#4568DC]">
+              {keyword}
+            </span>
+            {isLoading ? '검색중...' : <span>검색 결과 {pagination?.totalCount || 0}건</span>}
           </div>
+          <MdClose size={14} color="#bbb" className="cursor-pointer" onClick={onResetSearch} />
+        </ResultInfo>
+        <div className="h-fit">
+          {isLoading ? (
+            <div className="flex items-center justify-center border-t-[1px] border-solid border-[#bbb] p-4 text-sm text-[#bbb]">
+              검색중...
+            </div>
+          ) : data?.pages[0].places.length ? (
+            <div>
+              {data?.pages.map((page, pageNumber) =>
+                page.places.map(
+                  ({ id, place_name, road_address_name, category_group_name, phone }, idx) => (
+                    <ResultItem
+                      key={id}
+                      order={pageNumber * 15 + idx}
+                      placeName={place_name}
+                      address={road_address_name}
+                      category={category_group_name}
+                      phone={phone}
+                    />
+                  )
+                )
+              )}
+              <div ref={ref} />
+            </div>
+          ) : (
+            <div className="flex items-center justify-center border-t-[1px] border-solid border-[#bbb] p-4 text-sm text-[#bbb]">
+              검색 결과가 없습니다.
+            </div>
+          )}
         </div>
-      )}
+      </div>
     </>
   );
 };
