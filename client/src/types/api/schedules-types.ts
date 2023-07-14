bc01925caed75c2f057d583412826b629a1eb611
sungjihyun <jhsung23@gmail.com>
2023-07-15 02:35:54 +0900

    fix: 서버 응답에 맞게 타입 수정 (스케줄)

--- client/src/types/api/schedules-types.ts
+++ client/src/types/api/schedules-types.ts
@@ -1,15 +1,43 @@
-export interface Places {
+import { regions } from '@/datas/regions';
+import { CategoryGroupCode } from '../mapApi/place-types';
+
+export interface ScheduledPlace {
+  placeId: number;
+  scheduleId: number;
+  schedulePlaceId: number;
+  apiId: number;
   name: string;
   address: string;
-  latitude: number;
-  longitude: number;
-  stratDate: Date | string | number;
+  latitude: string;
+  longitude: string;
+  days: number;
+  orders: number;
+  bookmark: false;
+  category: CategoryGroupCode;
+}
+
+// GET : /schedules/:id
+export interface GetScheduleResponse {
+  scheduleId: number;
+  region: (typeof regions)[number];
+  title: string;
+  content: string;
+  memberCount: number;
+  startDate: Date;
+  endDate: Date;
+  createdAt: Date;
+  modifiedAt: Date;
+  memberId: number;
+  nickname: string;
+  places: ScheduledPlace[][];
 }
 
 // POST : /schedules/write
 export interface PostScheduleRequest {
-  content: string;
-  startDate: Date | string | number;
-  endDate: Date | string | number;
-  places: Places[];
+  title: string;
+  content: string | null;
+  region: (typeof regions)[number];
+  startDate: Date;
+  endDate: Date;
+  places: ScheduledPlace[][] | null;
 }
