55dfa6ec1b357098e792528da353453a5e654060
22yuu <22yuu@naver.com>
2023-07-16 01:20:15 +0900

    feat: 나의 일정 페이지 일정 카드 프레임

--- client/src/pages/MyTripPage.tsx
+++ client/src/pages/MyTripPage.tsx
@@ -1,10 +1,6 @@
-import { MypageSideNav } from '@/components';
-import Preparing from '@/components/common/Preparing';
+import { Button, MypageSideNav, SortingToolbar } from '@/components';
 import useAuthRedirect from '@/hooks/useAuthRedirect';
-import instance from '@/queries/axiosinstance';
-import { EMPTY_TOKEN } from '@/redux/slices/authSlice';
-import { useEffect } from 'react';
-import { Link, Navigate, useNavigate } from 'react-router-dom';
+import defaultImage from '../../public/region/seoul.webp';
 
 interface MyTripPageProps {}
 
@@ -15,7 +11,54 @@ const MyTripPage = ({}: MyTripPageProps) => {
   return (
     <div className=" flex">
       <MypageSideNav />
-      <Preparing />
+      <div className="flex w-full flex-col px-8 pt-12">
+        <SortingToolbar />
+        <div className="mt-6 flex flex-col">
+          <div className="mt-4 flex h-[220px] w-full rounded-lg border p-4 drop-shadow-lg">
+            <div id="img" className="mr-4 shrink-0">
+              <img src={defaultImage} alt="region" width={280} height={180} />
+            </div>
+            <div id="content" className="flex w-full flex-col border border-red-500">
+              <div className="flex w-full flex-1 border border-yellow-500">
+                <div className="flex-1 border border-blue-500"></div>
+                <div className="w-[120px]"></div>
+              </div>
+              <div className="flex h-[80px] w-full items-center justify-evenly border border-green-500">
+                <Button
+                  variant={'ring'}
+                  hovercolor={'default'}
+                  className="h-[50px] w-[120px] text-sm text-zinc-900"
+                >
+                  여행일지
+                </Button>
+                <Button
+                  variant={'ring'}
+                  hovercolor={'default'}
+                  className="h-[50px] w-[120px] text-sm text-zinc-900"
+                >
+                  일정상세보기
+                </Button>
+                <Button
+                  variant={'ring'}
+                  hovercolor={'default'}
+                  className="h-[50px] w-[120px] text-sm text-zinc-900"
+                >
+                  일정 수정
+                </Button>
+                <Button
+                  variant={'ring'}
+                  hovercolor={'default'}
+                  className="h-[50px] w-[120px] text-sm text-zinc-900"
+                >
+                  일정 공유
+                </Button>
+              </div>
+            </div>
+          </div>
+          <div className="mt-4 h-[220px] w-full rounded-lg border drop-shadow-lg"></div>
+          <div className="mt-4 h-[220px] w-full rounded-lg border drop-shadow-lg"></div>
+        </div>
+      </div>
     </div>
   );
 };
