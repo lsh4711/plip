8219c4eacf1b58fad926007f8a0a62ed2831c814
sungjihyun <jhsung23@gmail.com>
2023-07-17 15:23:16 +0900

    chore: 카테고리 한글명 추가

--- client/src/datas/categories.tsx
+++ client/src/datas/categories.tsx
@@ -34,3 +34,25 @@ export const categories: Record<(typeof CategoryNames)[number], Category> = {
     code: 'AD5',
   },
 };
+
+export const allCategories: Record<CategoryGroupCode, string> = {
+  '': '카테고리 없음',
+  MT1: '대형마트',
+  CS2: '편의점',
+  PS3: '어린이집, 유치원',
+  SC4: '학교',
+  AC5: '학원',
+  PK6: '주차장',
+  OL7: '주유소, 충전소',
+  SW8: '지하철역',
+  BK9: '은행',
+  CT1: '문화시설',
+  AG2: '중개업소',
+  PO3: '공공기관',
+  AT4: '관광명소',
+  AD5: '숙박',
+  FD6: '음식점',
+  CE7: '카페',
+  HP8: '병원',
+  PM9: '약국',
+};
