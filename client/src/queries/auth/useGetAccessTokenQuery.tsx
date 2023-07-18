d66ff8b89d4f0fc1f29d370646912a24a247100e
xion <calf-love@naver.com>
2023-07-18 14:59:14 +0900

    save

--- client/src/queries/auth/useGetAccessTokenQuery.tsx
+++ client/src/queries/auth/useGetAccessTokenQuery.tsx
@@ -1,18 +1,32 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import instance from '../axiosinstance';
+import { getAuthorizationHeader, isValidToken } from '@/utils/auth';
+import setAccessTokenToHeader from '@/utils/auth/setAccesstokenToHeader';
+import useInquireUsersQuery from './useInquireUsersQuery';
+import useSetAccessToken from '@/hooks/useSetAccessToken';
 
 const getAccessTokenAxios = async () => {
   const response = await instance.get('/api/tokens', { withCredentials: true });
+  const ACCESS_TOKEN = response.headers['authorization'];
+  setAccessTokenToHeader(ACCESS_TOKEN);
   return response;
 };
 
 const useGetAccessTokenQuery = () => {
+  const inquireQuery = useInquireUsersQuery();
+  const dispatchAccessToken = useSetAccessToken();
+
   const getAccessTokenQuery = useQuery({
     queryKey: ['getAccessToken'],
     queryFn: getAccessTokenAxios,
     suspense: true,
     retry: 1,
-    enabled: false,
+    enabled: !isValidToken(getAuthorizationHeader()),
+    staleTime: 1000 * 60 * 10,
+    onSuccess: (response) => {
+      inquireQuery.refetch();
+      dispatchAccessToken({ accesstoken: response.headers['authorization'] });
+    },
   });
 
   return getAccessTokenQuery;
