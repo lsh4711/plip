f65b9604241e13035d71c81beb10b75890ae86b0
xion <calf-love@naver.com>
2023-07-20 14:38:37 +0900

    :sparkles: 토큰재발급
    발급

---
+++ client/src/components/helper/AxiosProvider.tsx
@@ -0,0 +1,27 @@
+import useGetAccessTokenQuery from '@/queries/auth/useGetAccessTokenQuery';
+import instance from '@/queries/axiosinstance';
+import React from 'react';
+import { AxiosError } from 'axios';
+
+interface AxiosProviderProps {
+  children: React.ReactNode;
+}
+
+const AxiosProvider = ({ children }: AxiosProviderProps) => {
+  const getAccesstokenQuery = useGetAccessTokenQuery();
+  React.useEffect(() => {
+    instance.interceptors.response.use(
+      (response) => response,
+      async (error: AxiosError) => {
+        if (error.response && error.response.status === 403) {
+          const originalRequest = error.config;
+          await getAccesstokenQuery.refetch();
+        }
+        return Promise.reject(error);
+      }
+    );
+  }, []);
+  return <>{children}</>;
+};
+
+export default AxiosProvider;
