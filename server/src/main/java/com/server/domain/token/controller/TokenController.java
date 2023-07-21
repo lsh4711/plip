299dde337a7c49a9800816875cd8e8dffac0b1ee
Dayoung <dbsek21@naver.com>
2023-07-21 12:56:44 +0900

    chore: 회원이 없을 때, 리프레쉬 토큰 삭제

--- server/src/main/java/com/server/domain/token/controller/TokenController.java
+++ server/src/main/java/com/server/domain/token/controller/TokenController.java
@@ -37,6 +37,10 @@ public class TokenController {
             log.error("### 리프레쉬 토큰을 찾을 수 없음");
             jwtTokenizer.resetHeaderRefreshToken(response);
             throw new CustomException(ExceptionCode.REFRESH_TOKEN_NOT_FOUND);
+        } catch (CustomException ce){
+            log.error("### 해당 회원을 찾을 수 없음");
+            jwtTokenizer.resetHeaderRefreshToken(response);
+            throw new CustomException(ExceptionCode.MEMBER_NOT_FOUND);
         }
     }
 }
