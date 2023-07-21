8ab801cd2924ed8815ff8704226054a7ec5252b3
jeein2222 <jiinbag057@gmail.com>
2023-07-21 15:22:54 +0900

    refactor: 예최 처리 핸들러 메서드 추가

--- server/src/main/java/com/server/global/exception/GlobalExceptionHandler.java
+++ server/src/main/java/com/server/global/exception/GlobalExceptionHandler.java
@@ -4,39 +4,73 @@ import java.time.format.DateTimeParseException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler
-    public ResponseEntity customExceptionHandler(CustomException e) {
-        return ResponseEntity.status(e.getStatus()).body(e.getMessage());
-    }
+	@ExceptionHandler
+	public ResponseEntity<String> handleCustomExceptionHandler(CustomException e) {
+		return ResponseEntity.status(e.getStatus()).body(e.getMessage());
+	}
 
-    @ExceptionHandler
-    public ResponseEntity dateTimeParseExceptionHandler(DateTimeParseException e) {
-        return ResponseEntity.badRequest().body("날짜 형식이 올바르지 않습니다.");
-    }
+	//Date 형식이 올바르지 않을 때
+	@ExceptionHandler
+	public ResponseEntity<String> handleDateTimeParseExceptionHandler(DateTimeParseException e) {
+		return ResponseEntity.badRequest().body("날짜 형식이 올바르지 않습니다.");
+	}
 
-    @ExceptionHandler
-    public ResponseEntity handleMethodArgumentNotValidException(
-            MethodArgumentNotValidException e) {
-        Map<String, String> errorResponses = new HashMap<>();
-        List<FieldError> errors = e.getBindingResult().getFieldErrors();
+	//유효성 검사 실패 시
+	@ExceptionHandler
+	public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(
+		MethodArgumentNotValidException e) {
+		Map<String, String> errorResponses = new HashMap<>();
+		List<FieldError> errors = e.getBindingResult().getFieldErrors();
 
-        //stream 사용하지 않아도 되서 수정
-        errors.forEach(error -> errorResponses.put(error.getField(), error.getDefaultMessage()));
+		//stream 사용하지 않아도 되서 수정
+		errors.forEach(error -> errorResponses.put(error.getField(), error.getDefaultMessage()));
 
-        return ResponseEntity.badRequest().body(errorResponses);
-    }
+		return ResponseEntity.badRequest().body(errorResponses);
+	}
 
-    // @ExceptionHandler
-    public ResponseEntity exceptionHandler(Exception e) {
-        return ResponseEntity.internalServerError().body(e.getMessage());
-    }
+	//잘못된 HTTP 요청이 들어왔을 경우
+	@ExceptionHandler
+	public ResponseEntity<String> handleHttpRequestMethodNotSupportedException(
+		HttpRequestMethodNotSupportedException e) {
+		return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(e.getMessage());
+	}
+
+	//request body에 잘못된 데이터가 들어온 경우
+	@ExceptionHandler
+	public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+	}
+
+	//메서드 파라미터, 리턴 값에 문제가 있을 경우
+	@ExceptionHandler
+	public ResponseEntity<Set<ConstraintViolation<?>>> handleConstraintViolationException(
+		ConstraintViolationException e) {
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getConstraintViolations());
+	}
+
+	//RequestParam 이 누락되었을 경우
+	@ExceptionHandler
+	public ResponseEntity<String> handleMissingServletRequestParameterException(
+		MissingServletRequestParameterException e) {
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+	}
+
 }
