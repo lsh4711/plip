fed339619eadf2ac2e9991220febb43d0603bd1e
sungjihyun <jhsung23@gmail.com>
2023-07-21 20:12:05 +0900

    fix: 스크롤 시 사이드 내비도 함께 스크롤되는 이슈 해결

--- client/src/MypageLayout.tsx
+++ client/src/MypageLayout.tsx
@@ -6,7 +6,7 @@ export default function MypageLayout() {
   return (
     <div className="flex h-screen w-screen">
       <MypageSideNav />
-      <main className="flex h-full w-full flex-1 flex-col px-8 pb-12 pt-28">
+      <main className="flex h-full w-full flex-1 flex-col px-8 pb-12 pt-28 md:ml-56">
         <Outlet />
       </main>
     </div>
--- client/src/components/helper/MypageSideNav.tsx
+++ client/src/components/helper/MypageSideNav.tsx
@@ -9,43 +9,41 @@ const MypageSideNav = ({}: MypageSideNavProps) => {
   const path = useLocation();
   console.log(path);
   return (
-    <nav className="hidden h-screen w-56 shrink-0 border-r-2 px-2 md:block">
-      <div className="sticky left-0 top-[100px] flex w-full flex-col gap-2">
-        <span className="px-4 text-sm text-zinc-400">마이페이지</span>
-        {menus.map((menu) => {
-          if (menu.name === '마이페이지') return;
-          return (
-            <NavLink to={menu.route}>
-              {({ isActive }) => (
-                <div
-                  className="flex w-full gap-2 rounded-lg px-4 py-2"
-                  style={
-                    isActive
-                      ? { backgroundColor: '#4568DC20', color: '#4568DC', fontWeight: 600 }
-                      : {}
-                  }
-                >
-                  {menu.icon}
-                  {menu.name}
-                </div>
-              )}
-            </NavLink>
-          );
-        })}
-        <NavLink to="/mypage/signout">
-          {({ isActive }) => (
-            <div
-              className="flex gap-2 rounded-lg px-4 py-2"
-              style={
-                isActive ? { backgroundColor: '#4568DC20', color: '#4568DC', fontWeight: 600 } : {}
-              }
-            >
-              <FcDisapprove size={20} />
-              회원 탈퇴
-            </div>
-          )}
-        </NavLink>
-      </div>
+    <nav className="fixed left-0 top-[76px] hidden h-screen w-56 shrink-0 flex-col gap-2 border-r-2 px-2 pt-8 md:flex">
+      <span className="px-4 text-sm text-zinc-400">마이페이지</span>
+      {menus.map((menu) => {
+        if (menu.name === '마이페이지') return;
+        return (
+          <NavLink to={menu.route}>
+            {({ isActive }) => (
+              <div
+                className="flex w-full gap-2 rounded-lg px-4 py-2"
+                style={
+                  isActive
+                    ? { backgroundColor: '#4568DC20', color: '#4568DC', fontWeight: 600 }
+                    : {}
+                }
+              >
+                {menu.icon}
+                {menu.name}
+              </div>
+            )}
+          </NavLink>
+        );
+      })}
+      <NavLink to="/mypage/signout">
+        {({ isActive }) => (
+          <div
+            className="flex gap-2 rounded-lg px-4 py-2"
+            style={
+              isActive ? { backgroundColor: '#4568DC20', color: '#4568DC', fontWeight: 600 } : {}
+            }
+          >
+            <FcDisapprove size={20} />
+            회원 탈퇴
+          </div>
+        )}
+      </NavLink>
     </nav>
   );
 };
