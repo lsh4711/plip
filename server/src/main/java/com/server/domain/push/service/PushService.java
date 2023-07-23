178407c650dab54025fc45d9105e255e9a0d5539
lsh4711 <dowkwll@naver.com>
2023-07-24 00:12:40 +0900

    test

--- server/src/main/java/com/server/domain/push/service/PushService.java
+++ server/src/main/java/com/server/domain/push/service/PushService.java
@@ -1,14 +1,7 @@
 package com.server.domain.push.service;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import com.google.firebase.messaging.FirebaseMessaging;
-import com.google.firebase.messaging.FirebaseMessagingException;
-import com.google.firebase.messaging.Message;
-import com.google.firebase.messaging.Notification;
-import com.google.firebase.messaging.WebpushConfig;
-import com.google.firebase.messaging.WebpushFcmOptions;
 import com.server.domain.push.entity.Push;
 import com.server.domain.push.repository.PushRepository;
 
@@ -27,27 +20,27 @@ public class PushService {
         return savedPush;
     }
 
-    public String sendPush(String token) {
-        Message message = Message.builder()
-                .setToken(token)
-                .setNotification(Notification.builder()
-                        .setTitle("제목")
-                        .setBody("내용")
-                        .setImage("https://teamdev.shop/files/images/test?name=test")
-                        .build())
-                .setWebpushConfig(WebpushConfig.builder()
-                        .setFcmOptions(WebpushFcmOptions.withLink("https://teamdev.shop/"))
-                        .build())
-                .build();
+    // public String sendPush(String token) {
+    //     Message message = Message.builder()
+    //             .setToken(token)
+    //             .setNotification(Notification.builder()
+    //                     .setTitle("제목")
+    //                     .setBody("내용")
+    //                     .setImage("https://teamdev.shop/files/images/test?name=test")
+    //                     .build())
+    //             .setWebpushConfig(WebpushConfig.builder()
+    //                     .setFcmOptions(WebpushFcmOptions.withLink("https://teamdev.shop/"))
+    //                     .build())
+    //             .build();
 
-        try {
-            String response = FirebaseMessaging.getInstance().send(message);
-        } catch (FirebaseMessagingException e) {
-            log.error("### 푸시 에러", e.getMessage());
-            ExceptionCode
-            return ResponseEntity.internalServerError().body("푸시 알림 전송 실패");
-        }
+    //     try {
+    //         String response = FirebaseMessaging.getInstance().send(message);
+    //     } catch (FirebaseMessagingException e) {
+    //         log.error("### 푸시 에러", e.getMessage());
+    //         ExceptionCode
+    //         return ResponseEntity.internalServerError().body("푸시 알림 전송 실패");
+    //     }
 
-        return ResponseEntity.ok("푸시 알림 전송 성공");
-    }
+    //     return ResponseEntity.ok("푸시 알림 전송 성공");
+    // }
 }
