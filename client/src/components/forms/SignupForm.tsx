49f3e6bda68315a8b74d88a3d057bbb2cfa2112f
xion <calf-love@naver.com>
2023-07-24 10:45:58 +0900

    :bug: Fix
    fix

--- client/src/components/forms/SignupForm.tsx
+++ client/src/components/forms/SignupForm.tsx
@@ -58,6 +58,7 @@ const SignupForm = () => {
       })
       .catch(() => {
         setAuthCodeState({ disabled: true, message: '잠시 후 다시 시도해주세요' });
+        requestRef.current = false;
       });
   };
 
